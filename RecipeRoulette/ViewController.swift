20d014d4e4db14275a9bed018b05a5f01910b129
GTS Developer <gtsdeveloper@gtss-imac.local>
2016-04-07 10:02:27 -0400

    Initial Commit

---
+++ RecipeRoulette/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RecipeRoulette
+//
+//  Created by GTS Developer on 4/7/16.
+//  Copyright © 2016 GTS Developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
